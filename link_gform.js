562af6286582dabdf3be91f2eea58b0235e7440a
Raj181 <rajdattasunny@gmail.com>
2019-02-23 00:21:19 +0530

    Update link_gform.js

--- link_gform.js
+++ link_gform.js
@@ -1,1 +1,14 @@
+var $form = $('form#test-form'),
+    url = '****Your Google sheets request link****'
 
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    alert('Thanks for the submission');
+  );
+})
